95828160858af6c59b6f8ef48d5747a7388ea1e5
dbhujle <bhujle.Deepa@gmail.com>
2019-05-02 00:22:50 -0500

    addded more questions to trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,10 +7,16 @@ var losses = 0;
 
 // Initial object array of questions
 
-var triviaQuestions = [{question: "Bronze is an alloy consisting of two or more metals. What are they?", answer1: "Copper and zinc" , answer2: "Copper and tin", answer3: "Copper and gold" , answer4:"Copper and lead" , answerKey: "2"}, 
+var triviaQuestions = [ {question: "Bronze is an alloy consisting of two or more metals. What are they?", answer1: "Copper and zinc" , answer2: "Copper and tin", answer3: "Copper and gold" , answer4:"Copper and lead" , answerKey: "2"}, 
                         {question: "What colour is Cerulean?" , answer1: "Red" , answer2: "Green", answer3: "yellow" , answer4:"Blue" , answerKey: "4"}, 
                         {question: "What type of electrical charge does a neutron have?", answer1: "Negative" , answer2: "Positive", answer3: "No charge" , answer4:"Double negative" , answerKey: "3"}, 
                         {question: "The first sheep was successfully cloned in 1996, what was the sheep’s name?" , answer1: "Dolly" , answer2: "Donna", answer3: "Holly" , answer4:"Hailey" , answerKey: "1"}, 
+                        {question: "Mycology is the study of:" , answer1: "Insects" , answer2: "Water bodies", answer3: "Fungi" , answer4:"Volcanos" , answerKey: "3"}, 
+                        {question: "Air is mostly made up of:" , answer1: "Oxygen" , answer2: "Nitrogen ", answer3: "Carbon dioxide" , answer4:"Argon" , answerKey: "1"}, 
+                        {question: "What is the first element on the periodic table?" , answer1: "Sodium" , answer2: "Oxygen", answer3: "Helium" , answer4:"Hydrogen" , answerKey: "4"}, 
+                        {question: "Which side of your brain has more neurons?" , answer1: "Right" , answer2: "Left", answer3: "Both the same" , answer4:"None of the above" , answerKey: "2"}, 
+                        {question: "Which Desert dominates a large area of Northern Africa?" , answer1: "Red Sea" , answer2: "Aegean Sea", answer3: "Black Sea" , answer4:"Mediterranean Sea" , answerKey: "2"}, 
+                        {question: "What is the body of water that borders Greece, Turkey and Southern Italy?" , answer1: "Dolly" , answer2: "Donna", answer3: "Holly" , answer4:"Hailey" , answerKey: "1"}, 
                         ];
 
 window.onload = function() {
